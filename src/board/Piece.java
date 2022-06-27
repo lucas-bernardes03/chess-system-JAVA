91337dd45c257af10d6aca4198429b4d6f9896b3
Lucas Bernardes <lucasbernardes24@gmail.com>
2022-06-27 15:04:12 -0300

    board and pieces initial implementation

---
+++ src/board/Piece.java
@@ -0,0 +1,12 @@
+package board;
+
+public class Piece {
+    protected Position pos;
+    private Board board;
+
+    public Piece(Board board){ this.board = board; }
+
+    protected Board getBoard(){
+        return board;
+    }
+}
