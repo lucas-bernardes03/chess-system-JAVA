edf0a129f2f6d4724861f772d7e1a675dac94526
Lucas Bernardes <lucasbernardes24@gmail.com>
2022-06-30 15:14:41 -0300

    add castling move

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -2,13 +2,16 @@ package chess.pieces;
 
 import board.Board;
 import board.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Knight extends ChessPiece {
     
+    private ChessMatch chessMatch;
+
     public Knight(Board board, Color color) {
-        super(board, color);
+        super(board, color);;
     }
     
     @Override 
